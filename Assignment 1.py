d07a8935b64299645b950e11265fa2d278c4d855
v1khy17 <manojgarima1976@gmail.com>
2025-04-18 16:26:52 +0530

    Add files via upload

---
+++ Assignment 1.py
@@ -0,0 +1,35 @@
+#  TASK   1
+
+
+# get the input from user 
+
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+# perfrom mathematical operations
+addition = num1 + num2
+subtraction = num1 - num2
+multiplication =num1 *num2
+division = num1 / num2
+
+#Display results
+print(f"Addition = {num1} + {num2} = {addition}")
+print(f"Subtraction = {num1} - {num2}= {subtraction}")
+print(f"Multiplication = {num1} * {num2} = {multiplication}")
+print(f"Division = {num1} / { num2} = {division}")
+
+print( "thank you" )
+
+
+# Get user input for first and last name
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+
+# Concatenate first and last name
+full_name = first_name + " " + last_name
+
+# Print personalized greeting
+print(f"Hello, {full_name}! Welcome!to pthyon")
+
+ 
+ 
+ 
